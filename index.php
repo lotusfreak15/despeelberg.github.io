f33a39f930cae151eb96885c16b5aab06a5655d6
Thomas Toye <thomas@toye.io>
2017-05-10 17:58:36 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,198 @@
+<?php
+	date_default_timezone_set("Europe/Brussels");
+?>
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+
+    <script>
+        // account: speelpleindespeelberg@gmail.com
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-91687086-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Speelplein De Speelberg, Tomberg 21A, 8791 Beveren-Leie">
+    <meta name="author" content="Speelplein De Speelberg">
+    <link rel="shortcut icon" href="/assets/img/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="/assets/img/favicon.ico" type="image/x-icon">
+
+    <title>Speelplein De Speelberg - Beveren-Leie</title>
+
+    <!-- Kleuren => Blauw: #1a6ac1 Geel: #fed74c -->
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+	  <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="assets/js/hover.zoom.js"></script>
+    <script src="assets/js/hover.zoom.conf.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9&appId=865711616874725";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+    <!-- Static navbar -->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">De Speelberg</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#info">Info</a></li>
+            <li><a href="#kalender">Kalender</a></li>
+            <li><a href="https://www.facebook.com/speelpleinwerkingdespeelberg/" target="_blank">Facebook</a></li>
+            <li><a href="http://foto.despeelberg.be/#!home" target="_blank">Foto's</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+	<!-- +++++ Welcome Section +++++ -->
+	<div id="ww">
+	    <div class="container">
+			<div class="row">
+				<div class="col-lg-10 col-lg-offset-1 centered">
+					<img src="assets/img/user.png" alt="Logo">
+					<h1>Speelplein De Speelberg</h1>
+					<p>Speelplein De Speelberg is het Beverse Speelplein waar kinderen naar hartelust kunnen ravotten en spelen met hun vrienden! Onze speeltoestellen hebben nog veel te weinig slijtagesporen dus verwachten we je de komende vakantie's terug om te ravotten. </p>
+					<p>Kampen bouwen, knutselen, waterspelletjes, zandbak, schommels, gezelschapspelen of gewoon een intense voetbalmatch.Bij ons kun je het allemaal! Dagelijks staan ook een tiental animatoren klaar die elk een tweetal activiteiten aanbieden. Natuurlijk mag je nog altijd zelf kiezen of je hieraan meedoet of liever kampen bouwt, op de fietsen rijdt, gewoon chillt, ....</p>
+					<br>
+					<a class="iconfacebook" href="https://www.facebook.com/speelpleinwerkingdespeelberg/"><i class="fa fa-facebook fa-lg"></i></a>
+				</div><!-- /col-lg-8 -->
+			</div><!-- /row -->
+	    </div> <!-- /container -->
+	</div><!-- /ww -->
+
+	<!-- +++++ Info Section +++++ -->
+	<div class="container pt" id="info">
+		<div class="row">
+			<div class="col-lg-10 col-lg-offset-1 centered">
+				<h1>Info</h1>
+				<div class="col-lg-6 centered">
+					<h2>Waar?</h2>
+					<p>Onze speelpleinwerking kun je vinden op de terreinen op Tomberg 21A. We delen de gebouwen en het terrein met Chiro Leieland.</p>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2517.5724636627297!2d3.3391099!3d50.8761143!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c33c1c0a1d621f%3A0x4eb08a40b7484319!2sTomberg+21%2C+8791+Waregem!5e0!3m2!1snl!2sbe!4v1425072408049" width="450" height="350" frameborder="0" style="border:0"></iframe>
+				</div>
+				<div class="col-lg-6 centered">
+					<h2>Voor wie?</h2>
+					<p>Alle kinderen tussen 3 en 15 jaar zijn bij ons welkom.</p>
+					<h2>Door wie?</h2>
+					<p>Wij staan elke keer met een enthousiaste groep vrijwillige animatoren te springen!</p>
+					<h2>Hoeveel kost het?</h2>
+					<p>Onze speelpleinwerking bestaat uit verschillende dagdelen. Voor een voormiddag betaalt je 1 euro. Tijdens de middag kunnen de boterhammetjes van thuis worden opgegeten, dit kost ook 1 euro, in die prijs zit een drankje tijdens het eten inbegrepen. Een namiddag spelen kost 2 euro, en als vieruurtje wordt weer een drankje voorzien.</p>
+				</div>
+
+				<div class="col-lg-12">
+					<h2>Wanneer?</h2>
+					<p>
+					<b>7.45 - 12.00</b> In de zomervakantie is het speelplein de hele voormiddag open, dit kost 1 euro. De poort sluit om 9u30. <br />
+          			<b>12.00 - 13.00</b> Blijven over de middag is ook mogelijk, je brengt gewoon je boterhammen en 1 euro extra mee. <br />
+          			<b>13.00 - 13.45</b> De poort staat open voor iedereen, een namiddag spelen kost 2 euro. Er is een drankje voorzien. <br />
+          			<b>13.45 - 17.30</b> De poort sluit om 13.45. Om 17.30 gaat iedereen naar huis. <br /> <br />
+          			<b>Paasvakantie</b> Tijdens de paasvakantie is het speelplein enkel open in de namiddag <br />
+          			<b>Grote vakantie</b> Tijdens de grote vakantie is het speelplein open zowel in de voormiddag als in de namiddag, en dat gedurende de eerste 2 weken van juli en de laatste 2 weken van augustus.</p>
+          		</div>
+          	</div><!-- /col-lg-8 -->
+		</div><!-- /row -->
+	</div> <!-- /container -->
+
+	<!-- Kalender Section -->
+	<div id="kalender">
+	    <div class="container">
+			<div class="row">
+				<div class="col-lg-10 col-lg-offset-1 centered">
+					<h1>Kalender</h1>
+
+                    <div class="row">
+                        <h2>Paasvakantie</h2>
+                        <p>In de paasvakantie zijn we er opnieuw, dit in de tweede week. Onze poort staat open van 10 tot en met 14 april.
+                    </div>
+
+
+                    <div class="row">
+                        <h2>Zomervakantie</h2>
+                        <p>In de zomervakantie zijn we terug van de partij! We zijn open tijdens de eerste twee weken van juli (3 tot en met 7 juli, 10 tot en met 14 juli) en de laatste 3 weken van augustus (14 tot en met 18 augustus, 21 tot en met 25 augustus, 28 tot en met 31 augustus).
+                    </div>
+				</div><!-- /col-lg-8 -->
+			</div><!-- /row -->
+	    </div> <!-- /container -->
+	</div><!-- /ww -->
+
+    <div class="container pt" id="facebook">
+        <div class="row mt centered">
+            <h1>Facebook</h1>
+
+            <div class="row">
+                <h2>Op de hoogte blijven van de laatste nieuwtjes uit Speelbergland?</h2>
+
+                <p>Like dan snel <a href="https://www.facebook.com/speelpleinwerkingdespeelberg/">onze Facebookpagina</a>!
+            </div>
+
+            <div class="fb-page" data-href="https://www.facebook.com/speelpleinwerkingdespeelberg" data-tabs="timeline" data-width="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/speelpleinwerkingdespeelberg" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/speelpleinwerkingdespeelberg">Speelplein De Speelberg</a></blockquote></div>
+        </div><!-- /row -->
+    </div>
+
+
+    <!-- +++++ Footer Section +++++ -->
+
+	<div id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4">
+					<h4>Adres</h4>
+					<p>Tomberg 21A<br/>8791 Beveren-Leie<br/></p>
+				</div><!-- /col-lg-4 -->
+
+				<div class="col-lg-4">
+				</div><!-- /col-lg-4 -->
+
+				<div class="col-lg-4 text-right">
+					<h4>Contact</h4>
+					<p>info@despeelberg.be</p>
+					<p>Thomas Toye: 0478 78 60 81</p>
+				</div><!-- /col-lg-4 -->
+				<span>All rights reserved © <?php echo date("Y");?></span>
+			</div>
+		</div>
+	</div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
